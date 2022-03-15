8cd9c41597d04efcc41d27e59de496a3dd72db36
unknown <rodrigo.domingues.87@hotmail.com>
2022-03-15 11:01:26 -0300

    UserRepository

--- src/main/java/br/com/alura/mvc/mudi/repository/UserRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package br.com.alura.mvc.mudi.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.com.alura.mvc.mudi.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+
+	User findByUsername(String username);
+
+	
 }
